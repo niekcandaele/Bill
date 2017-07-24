a1eea482894b909622efdd4af1cf87b77493f171
niekcandaele <niekcandaele@gmail.com>
2017-07-24 12:22:47 +0200

    merged day7 and players command

--- src/bill.js
+++ src/bill.js
@@ -37,77 +37,83 @@ client.on('message', message => {
 // 7 DAYS COMMAND
 client.on('message', message => {
   if (message.content === '!day7') {
+    var onlinePlayers;
+    var date = new Date();
+    var days, minutes, hours, players, hostiles, animals;
+    var embed
+
+    function getOnlinePlayers(callback) {
+      var onlinePlayerList = "";
+      request('http://193.70.81.12:28248/api/getplayerslocation', function(error, response, body) {
+        var data = JSON.parse(body);
+        for (var i = 0; i < data.length; i++) {
+          var player = data[i];
+          if (player.online == true) {
+            onlinePlayerList += player.name + ", ";
+          }
+        }
+        //console.log("Function online players: " + onlinePlayerList);
+        return callback(onlinePlayerList);
+      });
+    }
+
     request('http://193.70.81.12:28248/api/getstats', function(error, response, body) {
       var body = JSON.parse(body);
-      var date = new Date();
-
-      var embed = {
-        "content": "7 Days",
-        "embed": {
-          "title": "7 Days",
-          "url": "https://discordapp.com",
-          "color": 15312430,
-          "timestamp": date,
-          "footer": {
-            "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
-            "text": "Bill"
-          },
-          "thumbnail": {
-            "url": client.displayAvatarURL
-          },
-          "author": {
-            "name": client.username,
+      days = body.gametime.days;
+      hours = body.gametime.hours;
+      minutes = body.gametime.minutes;
+      players = body.players;
+      animals = body.animals;
+      hostiles = body.hostiles;
+      onlinePlayers = getOnlinePlayers(function(onlinePlayers) {
+        embed = {
+          "content": "7 Days",
+          "embed": {
+            "title": "7 Days",
             "url": "https://discordapp.com",
-            "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
-          },
-          "fields": [{
-              "name": "Gametime",
-              "value": body.gametime.days + " days " + body.gametime.hours + " hours " + body.gametime.minutes + " minutes",
-              "inline": true
+            "color": 15312430,
+            "timestamp": date,
+            "footer": {
+              "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
+              "text": "Bill"
             },
-            {
-              "name": "Players",
-              "value": body.players,
-              "inline": true
+            "thumbnail": {
+              "url": client.displayAvatarURL
             },
-            {
-              "name": "----- ",
-              "value": "----- "
+            "author": {
+              "name": client.username,
+              "url": "https://discordapp.com",
+              "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
             },
-            {
-              "name": "Hostiles",
-              "value": body.hostiles,
-              "inline": true
-            },
-            {
-              "name": "Animals",
-              "value": body.animals,
-              "inline": true
-            },
-          ]
+            "fields": [{
+                "name": "Gametime",
+                "value": days + " days " + hours + " hours " + minutes + " minutes",
+                "inline": true
+              },
+              {
+                "name": "Players",
+                "value": players,
+                "inline": true
+              },
+              {
+                "name": "Online players",
+                "value": onlinePlayers
+              },
+              {
+                "name": "Hostiles",
+                "value": hostiles,
+                "inline": true
+              },
+              {
+                "name": "Animals",
+                "value": animals,
+                "inline": true
+              },
+            ]
+          }
         }
-      }
-      message.channel.send(embed);
+        message.channel.send(embed);
+      })
     });
   }
-
-});
-
-client.on('message', message => {
-  if (message.content === '!players') {
-    request('http://193.70.81.12:28248/api/getplayerslocation', function(error, response, body) {
-      var data = JSON.parse(body);
-      var onlinePlayerList = "";
-      var onlinePlayers = 0;
-      for (var i = 0; i < data.length; i++) {
-        var player = data[i];
-        if (player.online == true) {
-          onlinePlayerList += player.name + ", ";
-          onlinePlayers += 1;
-        }
-      }
-      message.channel.send("There are currently " + onlinePlayers + " players online! \n" + onlinePlayerList);
-    });
-  }
-
 });
