42c765dcb27fc605673154a3e9b16b1e2977d214
Niek Candaele <niekcandaele@gmail.com>
2017-08-14 05:57:44 +0200

    Fixed issue detected NaN for commands ran

--- src/commands/config/botinfo.js
+++ src/commands/config/botinfo.js
@@ -24,47 +24,8 @@ class BotInfo extends Commando.Command {
         var bootTime = await client.botStats.get("bootTime");
         var amountGuilds = await client.guilds.size;
         var amountUsers = await client.users.size;
-        /*
-@@@@@@@@@@@@ !!!!!!!!!!!!  curl https://api.github.com/repos/niekcandaele/Bill/git/refs/heads/master
-{
-  "ref": "refs/heads/master",
-  "url": "https://api.github.com/repos/niekcandaele/Bill/git/refs/heads/master",
-  "object": {
-    "sha": "deccaaac2313e506de0adb7388bfad36053459fd",
-    "type": "commit",
-    "url": "https://api.github.com/repos/niekcandaele/Bill/git/commits/deccaaac2313e506de0adb7388bfad36053459fd"
-  }
-}
-@@@@@@@@@@@@ !!!!!!!!!!!! curl https://api.github.com/repos/niekcandaele/Bill/git/commits/deccaaac2313e506de0adb7388bfad36053459fd
-{
-  "sha": "deccaaac2313e506de0adb7388bfad36053459fd",
-  "url": "https://api.github.com/repos/niekcandaele/Bill/git/commits/deccaaac2313e506de0adb7388bfad36053459fd",
-  "html_url": "https://github.com/niekcandaele/Bill/commit/deccaaac2313e506de0adb7388bfad36053459fd",
-  "author": {
-    "name": "Niek Candaele",
-    "email": "niekcandaele@gmail.com",
-    "date": "2017-08-02T22:50:21Z"
-  },
-  "committer": {
-    "name": "Niek Candaele",
-    "email": "niekcandaele@gmail.com",
-    "date": "2017-08-02T22:50:21Z"
-  },
-  "tree": {
-    "sha": "40507656a2bc57f31bffd3ecab86dd75a11313e8",
-    "url": "https://api.github.com/repos/niekcandaele/Bill/git/trees/40507656a2bc57f31bffd3ecab86dd75a11313e8"
-  },
-  "message": "Comma at end of online player list deleted",
-  "parents": [
-    {
-      "sha": "74c3419d8b2154273235a33a0b117ed396fbc3f9",
-      "url": "https://api.github.com/repos/niekcandaele/Bill/git/commits/74c3419d8b2154273235a33a0b117ed396fbc3f9",
-      "html_url": "https://github.com/niekcandaele/Bill/commit/74c3419d8b2154273235a33a0b117ed396fbc3f9"
-    }
-  ]
-}
-
-        */
+        console.log(cmdsRan);
+        console.log(typeof cmdsRan);
       } catch (e) {
         client.logger.error("COMMAND BOTINFO \n" + e);
         return msg.channel.send("Error loading data!");
@@ -77,7 +38,7 @@ class BotInfo extends Commando.Command {
       let time = process.uptime();
       let uptime = time.toString().toHHMMSS();
 
-      if (typeof cmdsRan !== "number") {
+      if (isNaN(cmdsRan)) {
         client.logger.debug("Type of cmdsRan: " + typeof cmdsRan);
         client.logger.error("BOTINFO CmdsRan == " + typeof cmdsRan + "--- Setting to 0");
         cmdsRan = 0;
